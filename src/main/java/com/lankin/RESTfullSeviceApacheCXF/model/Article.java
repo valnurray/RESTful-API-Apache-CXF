04fc5627a68fdcb7b46ba827962ea7ffcb79aea6
valnurray <valnurray@gmail.com>
2021-06-26 23:30:05 +0300

    added @XmlRootElement annotation to entity

--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/model/Article.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/model/Article.java
@@ -3,6 +3,8 @@ package com.lankin.RESTfullSeviceApacheCXF.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * Class {@code Article} in package {@code com.lankin.RESTfullSeviceApacheCXF.model}
@@ -16,18 +18,23 @@ import javax.persistence.*;
 @Data
 @Entity
 @Table(name = "article")
+@XmlRootElement
 public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @XmlElement
     private long id;
 
     @Column(name = "title", nullable = false)
+//    @XmlElement
     private String title;
 
     @Column(name = "author", nullable = false)
+//    @XmlElement
     private String author;
 
     @Column(name = "body")
+//    @XmlElement
     private String body;
 }
