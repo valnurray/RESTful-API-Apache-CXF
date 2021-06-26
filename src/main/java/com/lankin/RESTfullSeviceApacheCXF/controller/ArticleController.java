2e069c56d7016193465e382a73e9c4d8cb7cfd9a
valnurray <valnurray@gmail.com>
2021-06-26 16:19:11 +0300

    @POST

--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/controller/ArticleController.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/controller/ArticleController.java
@@ -1,5 +1,30 @@
 package com.lankin.RESTfullSeviceApacheCXF.controller;
 
+import com.lankin.RESTfullSeviceApacheCXF.model.Article;
+import com.lankin.RESTfullSeviceApacheCXF.service.ArticleService;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
 public class ArticleController {
+
+    private ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createCustomer(Article article) {
+        return Response.ok(articleService.saveArticle(article)).build();
+//        return  articleService.saveArticle(article);
+    }
 
 }
