8bdbf08d9bd34fe103325269f013fe94b441f97b
valnur <valnurray@gmail.com>
2021-07-06 12:33:35 +0300

    mapping dto

--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/mappers/ArticleMapper.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/mappers/ArticleMapper.java
@@ -4,8 +4,13 @@ package com.lankin.RESTfullSeviceApacheCXF.mappers;
 import com.lankin.RESTfullSeviceApacheCXF.model.Article;
 import com.lankin.RESTfullSeviceApacheCXF.service.models.request.ArticleRequest;
 import com.lankin.RESTfullSeviceApacheCXF.service.models.response.ArticleResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+@Mapper(componentModel = "spring")
 public interface ArticleMapper {
+
+    ArticleMapper INSTANCE = Mappers.getMapper( ArticleMapper.class );
 
     ArticleResponse ArticleToArticleResponse (Article article);
     Article ArticleResponseToArticle (ArticleResponse articleResponse);
--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/service/models/response/ArticleResponse.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/service/models/response/ArticleResponse.java
@@ -8,9 +8,6 @@ import javax.persistence.Id;
 @Data
 public class ArticleResponse {
 
-    @Id
-    private long id;
-
     private String title;
 
     private String author;
