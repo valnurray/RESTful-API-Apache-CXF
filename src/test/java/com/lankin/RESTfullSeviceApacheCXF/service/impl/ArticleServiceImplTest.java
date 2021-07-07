eaf586daf4e56a7d614571600b4ea7350573ab52
valnur <valnurray@gmail.com>
2021-07-07 16:37:10 +0300

    something work

--- src/test/java/com/lankin/RESTfullSeviceApacheCXF/service/impl/ArticleServiceImplTest.java
+++ src/test/java/com/lankin/RESTfullSeviceApacheCXF/service/impl/ArticleServiceImplTest.java
@@ -1,19 +1,30 @@
 package com.lankin.RESTfullSeviceApacheCXF.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lankin.RESTfullSeviceApacheCXF.ResTfullSeviceApacheCxfApplication;
+import com.lankin.RESTfullSeviceApacheCXF.configuration.CxfConfig;
 import com.lankin.RESTfullSeviceApacheCXF.repository.ArticleRepository;
+import org.apache.cxf.Bus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(ArticleServiceImpl.class)
+//@WebMvcTest(ArticleServiceImpl.class)
+//@WebMvcTest
+//@ContextConfiguration(classes = CxfConfig.class)
+//@SpringBootTest(classes = {ArticleServiceImpl.class})
+@SpringBootTest
 class ArticleServiceImplTest {
 
-    @Autowired
-    MockMvc mockMvc;
+//    @Autowired
+//    MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
