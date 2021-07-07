3576eb5e6cded2cb2a475606248d15817ee3ff48
valnur <valnurray@gmail.com>
2021-07-07 13:53:12 +0300

    Constants syntax's refactoring

--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/constants/Constants.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/constants/Constants.java
@@ -2,6 +2,6 @@ package com.lankin.RESTfullSeviceApacheCXF.constants;
 
 public class Constants {
     
-    public static final String MESSAGE_ARTICLE_NOT_FOUND = "error\":\"404\",\"message\":\"Article does not exist";
+    public static final String MESSAGE_ARTICLE_NOT_FOUND = "{\"error\":\"404\",\"message\":\"Article does not exist\"}";
 
 }
