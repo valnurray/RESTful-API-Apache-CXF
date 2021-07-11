1f5e88721153471c1e9f7e8b9fff5890a4e6f8db
valnurray <valnurray@gmail.com>
2021-07-11 16:22:32 +0300

    ddl, dml added

---
+++ dml.sql
@@ -0,0 +1,9 @@
+INSERT article (id, author, title, body)
+VALUES
+(1, 'Herbert George Wells', 'The War of the Worlds', 'body'),
+(2, 'Frank Herbert', 'DUNE', 'body'),
+(3, 'Clifford Donald Simak', 'A Choice of Gods', 'body'),
+(4, 'Isaac Asimov', 'The Bicentennial Man', 'body'),
+(5, 'For DELETE', 'DELETE OR UPDATE THIS VALUE', 'body');
+
+SELECT * FROM article;
