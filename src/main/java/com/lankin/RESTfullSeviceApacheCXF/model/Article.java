9b5f7e4b61afd875f2ad422ccd509b4a179f3534
valnur <valnurray@gmail.com>
2021-07-16 12:04:37 +0300

    added javax.validation.constraints. to entity

--- src/main/java/com/lankin/RESTfullSeviceApacheCXF/model/Article.java
+++ src/main/java/com/lankin/RESTfullSeviceApacheCXF/model/Article.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
@@ -29,13 +31,19 @@ public class Article {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "title", nullable = false)
+    @Column(name = "title")
+    @NotNull
+    @Size(max = 120, message = "title must be less then 120 characters")
     private String title;
 
-    @Column(name = "author", nullable = false)
+    @Column(name = "author")
+    @NotNull
+    @Size(max = 75, message = "author must be less then 75 characters")
     private String author;
 
     @Column(name = "body")
+    @Size(max = 255, message
+            = "body must be less then 255 characters")
     private String body;
 
 
