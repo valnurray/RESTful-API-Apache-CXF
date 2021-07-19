d927d8a3a215a76baf8e7df5063339b094d2edd4
valnur <valnurray@gmail.com>
2021-07-19 18:40:41 +0300

    change ddl

--- ddl.sql
+++ ddl.sql
@@ -1,9 +1,23 @@
 CREATE DATABASE IF NOT EXISTS lankin;
 USE lankin;
 
+-- CREATE TABLE article
+-- (id INT PRIMARY KEY AUTO_INCREMENT,
+--  author VARCHAR(75) NOT NULL,
+--  title VARCHAR(120) NOT NULL,
+--  body VARCHAR(255)
+-- );
+
+CREATE TABLE author
+(author_id INT PRIMARY KEY AUTO_INCREMENT,
+ firstName VARCHAR(120) NOT NULL,
+ lastName VARCHAR(120) NOT NULL,
+ `description` VARCHAR(255)
+);
+
 CREATE TABLE article
 (id INT PRIMARY KEY AUTO_INCREMENT,
- author VARCHAR(75) NOT NULL,
  title VARCHAR(120) NOT NULL,
- body VARCHAR(255)
+ body VARCHAR(255),
+ FOREIGN KEY (id) REFERENCES author(author_id)
 );
